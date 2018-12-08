cb93f1d8505afd8aca8e2df31cfcc5a4a7b7b29c
Nayra-Adel <nayra.adel128@stud.fci-cu.edu.eg>
2018-12-08 09:25:14 +0200

    Refactor #1 %79.425

--- titanic_dtree_kaggle.py
+++ titanic_dtree_kaggle.py
@@ -3,14 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-plt.rc("font", size=14)
-
-sns.set(style="white")
-sns.set(style="whitegrid", color_codes=True)
-
 # get titanic training & test file
 titanic_train = pd.read_csv("input/train.csv")
 titanic_test = pd.read_csv("input/test.csv")
@@ -18,22 +10,11 @@ titanic_test = pd.read_csv("input/test.csv")
 # keep the PassengerId just to use it in submission :)
 PassengerId = titanic_test.PassengerId.copy()
 
+# Survived Feature
+Y = titanic_train.Survived.copy()
+
 # check missing values in train data set
 print(titanic_train.isnull().sum())
-
-# what the 'Age' variable looks like.
-ax = titanic_train["Age"].hist(bins=15, color='teal', alpha=0.8)
-ax.set(xlabel='Age', ylabel='Count')
-plt.show()
-
-# what the 'Fare' variable looks like.
-ax = titanic_train["Fare"].hist(bins=50, color='teal', alpha=0.8)
-ax.set(xlabel='Fare', ylabel='Count')
-plt.show()
-
-# most passengers boarded in Southampton (S)
-sns.countplot(x='Embarked', data=titanic_train, palette='Set1')
-plt.show()
 
 '''
 Process the Data:
@@ -58,36 +39,31 @@ titanic_test.Age.fillna(titanic_test.Age.median(), inplace=True)
 titanic_test.Fare.fillna(titanic_test.Fare.median(), inplace=True)
 
 # create categorical variable
-train_data = pd.get_dummies(train_data, columns=["Pclass"])
-train_data = pd.get_dummies(train_data, columns=["Embarked"])
-titanic_test = pd.get_dummies(titanic_test, columns=["Pclass"])
-titanic_test = pd.get_dummies(titanic_test, columns=["Embarked"])
+
+# map the Embarked
+train_data['Embarked'] = train_data['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+titanic_test['Embarked'] = titanic_test['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
 
 # map the sex
 train_data.Sex = train_data.Sex.map({'male': 0, 'female': 1})
 titanic_test.Sex = titanic_test.Sex.map({'male': 0, 'female': 1})
 
-# Mapping Age in training data
 '''
 Converting Numerical Age to Categorical Variable
-    map:
-        child: 0
-        young: 1
-        adult: 2 
-        middle age: 3 
-        old age: 4
+    map => child: 0, young: 1, adult: 2, middle age: 3, old age: 4
 '''
-train_data.loc[train_data['Age'] <= 16, 'Age'] = 0
-train_data.loc[(train_data['Age'] > 16) & (train_data['Age'] <= 35), 'Age'] = 1
-train_data.loc[(train_data['Age'] > 35) & (train_data['Age'] <= 45), 'Age'] = 2
-train_data.loc[(train_data['Age'] > 45) & (train_data['Age'] <= 65), 'Age'] = 3
+# Mapping Age in training data
+train_data.loc[train_data['Age'] <= 12, 'Age'] = 0
+train_data.loc[(train_data['Age'] > 12) & (train_data['Age'] <= 20), 'Age'] = 1
+train_data.loc[(train_data['Age'] > 20) & (train_data['Age'] <= 40), 'Age'] = 2
+train_data.loc[(train_data['Age'] > 40) & (train_data['Age'] <= 65), 'Age'] = 3
 train_data.loc[train_data['Age'] > 65, 'Age'] = 4
 
 # Mapping Age in test data
-titanic_test.loc[titanic_test['Age'] <= 16, 'Age'] = 0
-titanic_test.loc[(titanic_test['Age'] > 16) & (titanic_test['Age'] <= 35), 'Age'] = 1
-titanic_test.loc[(titanic_test['Age'] > 35) & (titanic_test['Age'] <= 45), 'Age'] = 2
-titanic_test.loc[(titanic_test['Age'] > 45) & (titanic_test['Age'] <= 65), 'Age'] = 3
+titanic_test.loc[titanic_test['Age'] <= 12, 'Age'] = 0
+titanic_test.loc[(titanic_test['Age'] > 12) & (titanic_test['Age'] <= 20), 'Age'] = 1
+titanic_test.loc[(titanic_test['Age'] > 20) & (titanic_test['Age'] <= 40), 'Age'] = 2
+titanic_test.loc[(titanic_test['Age'] > 40) & (titanic_test['Age'] <= 65), 'Age'] = 3
 titanic_test.loc[titanic_test['Age'] > 65, 'Age'] = 4
 
 # Mapping Fare in training data
@@ -109,17 +85,23 @@ titanic_test.loc[titanic_test['Fare'] > fare_median, 'Fare'] = 3
 train_data.drop(['Name', 'PassengerId', 'Survived', 'Cabin'], axis=1, inplace=True)
 titanic_test.drop(['Name', 'PassengerId', 'Cabin'], axis=1, inplace=True)
 
-Y = titanic_train.Survived.copy()
-
 # split the data to train & validate
 X_train, X_valid, y_train, y_valid = train_test_split(train_data, Y, test_size=0.2, random_state=7)
 
-tree1 = tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=3, min_samples_leaf=20)
+tree1 = tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=3, min_samples_leaf=10)
 
 tree1.fit(X_train, y_train)
 
 print(round(tree1.score(X_train, y_train) * 100, 2))
 print(round(tree1.score(X_valid, y_valid) * 100, 2))
+
+# 79.425
+predict = tree1.predict(titanic_test)
+submission = pd.DataFrame({
+    "PassengerId": PassengerId,
+    "Survived": predict
+})
+submission.to_csv('AllFeatures_Except_Cabin_PassengerId_Name.csv', index=False)
 
 '''
 when i drop Fare & Embarked_Q still the same result:
@@ -146,12 +128,18 @@ Training : 81.88
 Validate : 79.89
 Kaggle   : 78.947
 
+max depth = 4
+min_samples_leaf = 1
+Training : 83.99
+Validate : 81.01
+Kaggle   : 79.425
+
+if i drop 'Fare' & 'Parch' still the same result
+max depth = 3
+min_samples_leaf = 10
+Training : 83.71
+Validate : 81.56
+Kaggle   : 79.425
+
 Using Cabin feature is useless
 '''
-
-predict = tree1.predict(titanic_test)
-submission = pd.DataFrame({
-    "PassengerId": PassengerId,
-    "Survived": predict
-})
-submission.to_csv('submission1.csv', index=False)
